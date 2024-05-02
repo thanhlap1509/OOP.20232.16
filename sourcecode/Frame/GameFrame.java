17f6fcb21497e4db4a959c467abda6947b2a37b9
Dinh Ngoc Lap Thanh <thanhdinhngoclap123@gmail.com>
2024-05-02 10:08:33 +0700

    complete turn rotation in header

--- sourcecode/Frame/GameFrame.java
+++ sourcecode/Frame/GameFrame.java
@@ -12,9 +12,18 @@ import java.io.IOException;
 import java.util.TimerTask;
 import java.util.Timer;
 public class GameFrame extends JFrame {
-    final int HEADER_SIZE = 120;
+    final int HEADER_SIZE = 110;
     final Color HEADER_COLOR = Color.cyan;
+    private JLabel player1Info;
+    private JLabel player2Info;
+    private JLabel timerLabel;
+    private JLabel turnDisplayed;
+    private Border paddingBorder;
+    private Border indicatorBorder;
+    private Border compoundBorder;
     private int secondCountDown;
+    private Timer timer;
+    private int turn;
     GameFrame() {
         // try-catch reading png file into image instances then convert to image icon
         BufferedImage p1BuffImg = null;
@@ -29,16 +38,17 @@ public class GameFrame extends JFrame {
         ImageIcon player1ImgIcon = new ImageIcon(player1Img);
         Image player2Img = p2BuffImg.getScaledInstance(HEADER_SIZE, HEADER_SIZE, Image.SCALE_DEFAULT);
         ImageIcon player2ImgIcon = new ImageIcon(player2Img);
-        Border yellowBorder = BorderFactory.createLineBorder(Color.yellow, 3);
+        paddingBorder =  BorderFactory.createEmptyBorder(0, 5, 0, 5);
+        indicatorBorder = BorderFactory.createLineBorder(Color.yellow, 2);
+        compoundBorder = BorderFactory.createCompoundBorder(indicatorBorder, paddingBorder);
         //player 1 container
-        JLabel player1Info = new JLabel();
+        player1Info = new JLabel();
         player1Info.setText("<html>Name_1<br>Points: 0</html>");
         player1Info.setIcon(player1ImgIcon);
-        player1Info.setBorder(yellowBorder);
         player1Info.setHorizontalTextPosition(JLabel.RIGHT);
         player1Info.setVerticalTextPosition(JLabel.CENTER);
         //player 2 container
-        JLabel player2Info = new JLabel();
+        player2Info = new JLabel();
         player2Info.setText("<html>Name_2<br>Points: 0</html>");
         player2Info.setIcon(player2ImgIcon);
         player2Info.setHorizontalTextPosition(JLabel.LEFT);
@@ -46,18 +56,17 @@ public class GameFrame extends JFrame {
         //game status container
         JPanel gameStatus = new JPanel();
         gameStatus.setLayout(new GridLayout(6, 1));
-        JLabel turnDisplayed = new JLabel();
-        turnDisplayed.setText("Current turn: Player 1");
+        turn = 1;
+        turnDisplayed = new JLabel();
         turnDisplayed.setBorder(new EmptyBorder(5, 5, 5, 5));
         turnDisplayed.setHorizontalAlignment(SwingConstants.CENTER);
         turnDisplayed.setVerticalAlignment(SwingConstants.TOP);
 
-        JLabel timerLabel = new JLabel();
-        timerLabel.setText("Time: 01:00");
+        timerLabel = new JLabel();
         timerLabel.setBorder(new EmptyBorder(5, 5, 5, 5));
         timerLabel.setHorizontalAlignment(SwingConstants.CENTER);
         timerLabel.setVerticalAlignment(SwingConstants.TOP);
-        timerCountDown(timerLabel);
+        timerCountDown();
         gameStatus.add(turnDisplayed);
         gameStatus.add(timerLabel);
         gameStatus.setBackground(HEADER_COLOR);
@@ -79,9 +88,19 @@ public class GameFrame extends JFrame {
         this.add(gameInfo, BorderLayout.NORTH);
         this.setVisible(true);
     }
-    private void timerCountDown(JLabel timerLabel){
-        Timer timer = new Timer();
-        secondCountDown = 60;
+    private void timerCountDown(){
+        timer = new Timer();
+        secondCountDown = 10;
+        if (turn == 1){
+            player2Info.setBorder(null);
+            player1Info.setBorder(compoundBorder);
+            turnDisplayed.setText("Current turn: " + "Name_1");
+        }
+        else {
+            player1Info.setBorder(null);
+            player2Info.setBorder(compoundBorder);
+            turnDisplayed.setText("Current turn: " + "Name_2");
+        }
         timer.scheduleAtFixedRate(new TimerTask() {
             public void run() {
                 if (secondCountDown >= 0) {
@@ -91,7 +110,10 @@ public class GameFrame extends JFrame {
                     secondCountDown--;
                 } else {
                     timer.cancel();
-                    System.out.println("Countdown finished.");
+                    System.out.println("Finish turn of player " + turn);
+                    if (turn == 1) turn = 2;
+                    else turn = 1;
+                    timerCountDown();
                 }
             }
         }, 0, 1000); // Run the task every second
